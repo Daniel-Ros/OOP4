73c0b8bafd14d635a7907f6ff6f6ad8e9f0c447e
Daniel-Ros <danimalau@live.com>
2021-12-21 19:20:21 +0200

    stated on the gui

--- src/Drawer.py
+++ src/Drawer.py
@@ -1,5 +1,8 @@
 import random
 
+import pygame_gui
+from pygame.time import Clock
+
 from GraphInterface import GraphInterface
 
 import pygame
@@ -12,6 +15,7 @@ class Drawer:
         self.min_y = float('inf')
         self.max_y = float('-inf')
         self.graph = g
+        self.UI = None
 
     def main(self):
         g = self.graph
@@ -20,8 +24,7 @@ class Drawer:
         # load and set the logo
 
         # create a surface on screen that has the size of 240 x 180
-        screen = pygame.display.set_mode((800, 800))
-
+        screen = pygame.display.set_mode((800, 800),pygame.RESIZABLE)
         # define a variable to control the main loop
         running = True
 
@@ -41,13 +44,30 @@ class Drawer:
             self.max_y += 0.001
             self.min_y -= 0.001
 
+        self.UI = pygame_gui.UIManager((800, 800))
+
+        hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((700, 50), (100, 50)),
+                                                    text='Say Hello',
+                                                    manager=self.UI)
+
+        input = pygame_gui.elements.UIWindow(,)
+
+        clock = Clock()
         while running:
+            time_delta = clock.tick(60) / 1000.0
             # event handling, gets all event from the event queue
             for event in pygame.event.get():
                 # only do something if the event is of type QUIT
                 if event.type == pygame.QUIT:
                     # change the value to False, to exit the main loop
                     running = False
+
+                if event.type == pygame.USEREVENT:
+                    if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                        if event.ui_element == hello_button:
+                            print('Hello World!')
+
+                self.UI.process_events(event)
 
             screen.fill((255,255,255),pygame.display.get_surface().get_rect())
             for n in nodes:
@@ -57,6 +77,12 @@ class Drawer:
                     for e in g.all_out_edges_of_node(n.id):
                         pto = self.point_to_screen_cord(float(g.get_all_v()[e].loc[0]),float(g.get_all_v()[e].loc[1]))
                         pygame.draw.line(screen,(0,0,0),pfrom,pto)
+
+
+
+            self.UI.update(time_delta)
+
+            self.UI.draw_ui(screen)
 
             pygame.display.update()
 
